e383b05577b090c32a0706b16874e76ce01198d5
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-18 20:32:39 -0400

    feat: ✨ add public area

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Repository;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view("welcome", ["repositories" => Repository::all()]);
+    }
+}
